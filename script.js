7b74fce4cce29bda83269a6a93136a2ac242d782
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-25 17:05:16 +0100

    Almost finished 2nd exercise

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//first exercise
+
 const productAddedToCartAlert = () => {
   alert("Product added to cart.");
 };
@@ -12,4 +14,24 @@ const buyButton = document.getElementById("buy-button");
 
 buyButton.onclick = function () {
   changingButtonLabel();
+};
+
+//second exercise
+
+const checkingCredentials = () => {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  if (password === login) {
+    message.textContent = "Hey " + login + ", welcome to your profile.";
+  } else {
+    message.textContent = "Hey " + login + ", you forgot the password again.";
+  }
+};
+
+const submit = document.getElementById("submit");
+
+submit.onclick = function () {
+  checkingCredentials();
 };
