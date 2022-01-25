1bf59413208002194bac86c87bcdcfee027ec80f
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-25 16:31:41 +0100

    Changing the label of the button.

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const productAddedToCartAlert = () => {
   alert("Product added to cart.");
 };
 
+const changingButtonLabel = () => {
+  buyButton.textContent = "In cart.";
+};
+
 const buyButton = document.getElementById("buy-button");
 
 buyButton.onclick = function () {
-  productAddedToCartAlert();
+  changingButtonLabel();
 };
