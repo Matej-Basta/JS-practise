b6d323260b4e2b9d85f67d21fbbde616d0987900
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-26 16:40:11 +0100

    Third exercise finished

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ buyButton.onclick = function () {
 const checkingCredentials = () => {
   const login = document.getElementById("login").value;
   const password = document.getElementById("password").value;
-  const message = document.getElementById("message");
+  let message = document.getElementById("message");
 
   if (password === login) {
     message.textContent = "Hey " + login + ", welcome to your profile.";
@@ -34,4 +34,21 @@ const submit = document.getElementById("submit");
 
 submit.onclick = function () {
   checkingCredentials();
+};
+
+//third exercise
+let bulb = document.getElementById("bulb-container");
+
+const changingBackground = () => {
+  if (bulb.className === "bulb-container bulb-container--on") {
+    bulb.className = "bulb-container";
+    console.log("both");
+  } else {
+    bulb.className += " bulb-container--on";
+    console.log("one");
+  }
+};
+
+bulb.onclick = function () {
+  changingBackground();
 };
